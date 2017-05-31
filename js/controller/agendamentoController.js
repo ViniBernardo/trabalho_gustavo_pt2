cd2c66af891ac9ebb209c61584c942c55e6a233a
vinibernardo <vinicius_pg@hotmail.com.br>
2017-05-31 19:57:22 -0400

    novo

--- js/controller/agendamentoController.js
+++ js/controller/agendamentoController.js
@@ -1,18 +1,49 @@
-angular.module("singlepageapp").controller("agendamentoController", function ($scope, $location) {
+angular.module("singlepageapp").controller("agendamentoController", function ($scope,APIService, $location) {
 
 
-    $scope.data = new Date();
+    $scope.arduino = [];
 
+    $scope.irPara = function(caminho){
+        $location.url(caminho);
+    };
 
-    $scope.imprimirDataEHorario = function(d){
-        alert(d);
+    $scope.salvar = function(tabela_anduino){
+
+        APIService.salvar(tabela_anduino).then(function(tabela_anduino){
+            alert("cadastrada com sucesso");
+            $location.url("/adicionaAgendamentos");
+        },function(err){
+            alert("Deu tilt");
+        });
     }
 
-    $scope.irPara = function (caminho) {
-        $location.url(caminho);
+
+
+
+    var listar = function(){
+
+        var sucesso = function(dados){
+            $scope.arduino = dados.data;
+        };
+
+        var erro = function(err){
+            alert("Erro"+err);
+        };
+
+        APIService.listar().then(sucesso,erro);
     };
 
 
 
 
+
+
+    listar();
+
+
+
+
 });
+
+
+
--- js/service/APIService.js
+++ js/service/APIService.js
@@ -1,3 +1,36 @@
-/**
- * Created by Vinicius on 15/05/2017.
- */
+
+angular.module("singlepageapp").factory("APIService",
+    function($http){
+
+        var _listar = function(){
+            return $http({
+                method:"GET",
+                url: "http://localhost:8080/anduino/cliente",
+            });
+        };
+
+
+        var _salvar = function(dados){
+            return $http({
+                method:"POST",
+                url: "http://localhost:8080/anduino/cliente",
+                data:dados
+            });
+        };
+        //--------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+        return {
+
+            listar: _listar,
+            salvar: _salvar
+
+
+        }
+    });
