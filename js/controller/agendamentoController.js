32749eee20750828cd95410c7a752f395bdbfa07
vinibernardo <vinicius_pg@hotmail.com.br>
2017-06-20 21:14:10 -0400

    trabalho

--- js/controller/agendamentoController.js
+++ js/controller/agendamentoController.js
@@ -1,3 +1,4 @@
+
 angular.module("singlepageapp").controller("agendamentoController", function ($scope,APIService, $location) {
 
 
@@ -7,18 +8,40 @@ angular.module("singlepageapp").controller("agendamentoController", function ($s
         $location.url(caminho);
     };
 
-    $scope.salvar = function(tabela_anduino){
 
-        APIService.salvar(tabela_anduino).then(function(tabela_anduino){
+
+
+    $scope.salvar = function(curso){
+
+        APIService.salvar(curso).then(function(curso){
             alert("cadastrada com sucesso");
-            $location.url("/adicionaAgendamentos");
+            $location.url("/agendamentos");
         },function(err){
             alert("Deu tilt");
         });
     }
 
+    $scope.delete = function(id){
+
+        APIService.delete(id).then(function(id){
+        },function(){
+            $location.url("/agendamentos");
+        });
+    }
 
 
+    var listar = function(){
+
+        var sucesso = function(dados){
+            $scope.arduino = dados.data;
+        };
+
+        var erro = function(err){
+            alert("Erro"+err);
+        };
+
+        APIService.listar().then(sucesso,erro);
+    };
 
     var listar = function(){
 
@@ -35,7 +58,19 @@ angular.module("singlepageapp").controller("agendamentoController", function ($s
 
 
 
+    $scope.edita = function (curso) {
+        //alert(curso);
+        var sucesso = function (dados) {
+            $scope.aul = dados.data;
+        };
 
+        var erro = function (err) {
+            alert("Erro ao listar os alunos");
+        };
+
+        APIService.listarAlunoPorCurso(curso).then(sucesso,erro);
+
+    }
 
 
     listar();
--- js/service/APIService.js
+++ js/service/APIService.js
@@ -1,4 +1,3 @@
-
 angular.module("singlepageapp").factory("APIService",
     function($http){
 
@@ -6,30 +5,38 @@ angular.module("singlepageapp").factory("APIService",
             return $http({
                 method:"GET",
                 url: "http://localhost:8080/anduino/cliente",
+
             });
         };
+
+
+        //--------------------------------------------------------------------------------
+
 
 
         var _salvar = function(dados){
             return $http({
                 method:"POST",
-                url: "http://localhost:8080/anduino/cliente",
+                url: " http://localhost:8080/anduino/cliente/",
                 data:dados
             });
         };
-        //--------------------------------------------------------------------------------
 
-
-
-
-
+        var _delete = function(id){
+            return $http({
+                method:"DELETE",
+                url: " http://localhost:8080/anduino/cliente/"+id,
+                data:id
+            });
+        };
 
 
 
         return {
 
             listar: _listar,
-            salvar: _salvar
+            salvar: _salvar,
+            delete:_delete
 
 
         }
